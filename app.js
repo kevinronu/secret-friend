3b956f28a7e76c2a1ed132fe4890a6fa3c612d5d
Kevin Robles <kevinronu@gmail.com>
2025-09-18 22:35:33 -0500

    Feat: Add pickRandomFriend function

--- app.js
+++ app.js
@@ -25,3 +25,39 @@ function addFriend() {
 
   input.value = "";
 }
+
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+}
+
+function cleanFriendsList() {
+  friendNames = [];
+
+  const unorderedList = document.querySelector(".name-list");
+  unorderedList.replaceChildren();
+}
+
+function setResult(selectedFriendName) {
+  const li = document.createElement("li");
+  li.textContent = `The secret friend is: ${selectedFriendName}`;
+
+  const unorderedList = document.querySelector(".result-list");
+  unorderedList.appendChild(li);
+}
+
+function pickRandomFriend() {
+  if (friendNames.length == 0) {
+    alert("At least one friend name is required");
+
+    return;
+  }
+
+  const randomIndex = getRandomInt(0, friendNames.length - 1);
+  const selectedFriendName = friendNames[randomIndex];
+
+  cleanFriendsList();
+  setResult(selectedFriendName);
+}
